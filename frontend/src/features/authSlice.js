3f0a30da4fe5db6aa154f9c83a75196310ee514b
Nurena67 <arisnurena@gmail.com>
2024-12-29 18:24:34 +0700

    update .env

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axiosInstance from "./axios";
+import axiosInstance from "./axios.js";
 
 const initialState = {
     user: null,
--- frontend/src/features/axios.js
+++ frontend/src/features/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "https://web-app-auth-production.up.railway.app",
+    baseURL: "https://web-app-auth-production.up.railway.app",
     withCredentials: true
 });
 
