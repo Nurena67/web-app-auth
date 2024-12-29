a8b95e0d01f00043261bd37188c4fc1dbe32eac0
Nurena67 <arisnurena@gmail.com>
2024-12-29 12:07:09 +0700

    Edit Frontend

--- frontend/src/features/axios.js
+++ frontend/src/features/axios.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL || "https://web-app-auth-production.up.railway.app",
+    withCredentials: true
 });
 
 export default axiosInstance;
