fa8f23b4cca4727f687de3a53e4acb1878db1f11
Nurena67 <arisnurena@gmail.com>
2024-12-30 20:59:10 +0700

    edit authSlice.js

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -45,30 +45,30 @@ export const authSlice = createSlice({
         reset: (state) => initialState
     },
     extraReducers:(builder) =>{
-        builder.addCase(LoginUser.pending, (state) =>{
+        builder.addCase(login.pending, (state) =>{
             state.isLoading = true;
         });
-        builder.addCase(LoginUser.fulfilled, (state, action) =>{
+        builder.addCase(login.fulfilled, (state, action) =>{
             state.isLoading = false;
             state.isSuccess = true;
             state.user = action.payload;
         });
-        builder.addCase(LoginUser.rejected, (state, action) =>{
+        builder.addCase(login.rejected, (state, action) =>{
             state.isLoading = false;
             state.isError = true;
             state.message = action.payload;
         })
 
         // Get User Login
-        builder.addCase(getMe.pending, (state) =>{
+        builder.addCase(checkLogin.pending, (state) =>{
             state.isLoading = true;
         });
-        builder.addCase(getMe.fulfilled, (state, action) =>{
+        builder.addCase(checkLogin.fulfilled, (state, action) =>{
             state.isLoading = false;
             state.isSuccess = true;
             state.user = action.payload;
         });
-        builder.addCase(getMe.rejected, (state, action) =>{
+        builder.addCase(checkLogin.rejected, (state, action) =>{
             state.isLoading = false;
             state.isError = true;
             state.message = action.payload;
