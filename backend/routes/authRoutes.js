1bb26666df0f0d740eeedb2feaa73cce61e137b5
Nurena67 <arisnurena@gmail.com>
2025-01-03 10:44:27 +0700

    update Backend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,6 @@
 import express from "express";
 import {Login, logOut, Me} from "../controllers/auth.js";
-<<<<<<< HEAD
-import {verifyUser} from '../middleware/AuthUser.js'
-=======
 import { verifyUser } from "../middleware/AuthUser.js";
->>>>>>> 2fac3fd1f62df1b1f09f93fc012edae191582790
 
 const router = express.Router();
 
