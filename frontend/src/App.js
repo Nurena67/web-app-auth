37eb4dfd6a213a77c26a9c883e8559263991fbe5
Nurena67 <arisnurena@gmail.com>
2025-01-03 18:10:41 +0700

    edit Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { checkLogin } from "./features/authSlice";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Dashboard from "./pages/Dashboard";
@@ -10,12 +13,20 @@ import AddPatient from "./pages/AddPatient";
 import DetailPatient from "./pages/DetailPatient";
 import EditPatient from "./pages/EditPatient";
 function App() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      dispatch(checkLogin());
+    }
+  }, [dispatch]);
   return (
     <div>
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/login" element={ user? <Navigate to='/dashboard'/> : <Login/>} />
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/patients" element={<Patients/>}/>
         <Route path="/users" element={<Users/>}/>
