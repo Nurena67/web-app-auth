89c6921bb5e45132d62008224327fcd0e3bf2190
Nurena67 <arisnurena@gmail.com>
2025-01-10 21:36:50 +0700

    Update patientControllers

--- frontend/src/components/PatientList.jsx
+++ frontend/src/components/PatientList.jsx
@@ -62,7 +62,7 @@ const PatientList = () => {
                                     <td>{patient.name}</td>
                                     <td>{patient.complaint}</td>
                                     <td>{patient.doctorName}</td>
-                                    <td>{patient.nurseNames}</td>
+                                    <td>{patient.nurseNames.join(', ')}</td>
                                     <td>
 
                                     <Link to={`/patients/${patient.medicalRecordNumber}`} className='button is-small is-primary' > Detail </Link>
