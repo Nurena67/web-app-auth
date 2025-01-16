159958f33ac389a6ae81781076f54c7892050452
Nurena67 <arisnurena@gmail.com>
2025-01-16 22:13:50 +0700

    update verifyEmail page

--- frontend/src/components/VerifyEmail.jsx
+++ frontend/src/components/VerifyEmail.jsx
@@ -1,33 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
 import axios from 'axios';
-import { useSearchParams, useNavigate } from 'react-router-dom';
 
 const VerifyEmail = () => {
-  const [searchParams] = useSearchParams();
+  const { token } = useParams();
   const navigate = useNavigate();
-  const token = searchParams.get('token');
   const [message, setMessage] = useState('');
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const verifyToken = async () => {      
       try {
-        const response = await axios.post('https://web-app-auth.up.railway.app/verify-email',{token});
-        if (response.data.success) {
+        const response = await axios.get(
+          `https://web-app-auth.up.railway.app/verify-email/${token}`
+          );
+        if (response.status === 200) {
           setMessage('Email successfully verified! You can now log in.');
         } else {
-          setMessage(response.data.message || 'Verification failed.');
+          setError(true);
+          setMessage('Verification failed.');
         }
       } catch (error) {
+        setError(true);
         setMessage('An error occurred during verification. Please try again.');
       }
     };
 
     if (token) {
       verifyToken();
-    } else {
-      setMessage('Invalid verification link.');
     }
-
   }, [token]);
 
   const goLogin = () => {
@@ -36,11 +38,13 @@ const VerifyEmail = () => {
 
   return (
     <div className="container">
-      <h1>Email Verification</h1>
-      <p>{message}</p>
+      <div className="notification is-primary">
+        <h1 className="title">{error ? "Verification Failed" : "Verification Successful"}</h1>
+        <p>{message}</p>
+      </div>
       <button onClick={goLogin} className="button is-link is-medium">
                 Back to Login Page
-        </button>
+      </button>
     </div>
   );
 };
