40b0cac707735aaf3718efd893ad9dde9ab98d5e
Nurena67 <arisnurena@gmail.com>
2025-01-22 10:35:45 +0700

    edit Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,17 +3,13 @@ import Layout from "./Layout";
 import Welcome from "../components/Welcome";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { checkLogin} from "../features/authSlice";
+
 
 const Dashboard = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
   const { isError } = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    dispatch(checkLogin());
-  }, [dispatch]);
-
+ 
   useEffect(() => {
     if (isError) {
       navigate("/");
